68def6a1da35658f11302ef38ef9ef2181bb1cca
RavilGit <sefan.wen9.net@mail.ru>
2022-11-27 23:10:54 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace VigenereCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 75);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(446, 475);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(469, 74);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(446, 475);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 45);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(296, 24);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Кириллица (латиница по умолчанию)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(232, 9);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(503, 29);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ключ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(753, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 29);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 561);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Шифр Вижинера";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private CheckBox checkBox1;
+        private TextBox textBox3;
+        private Label label1;
+        private Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+using System.Text;
+
+namespace VigenereCipher
+{
+    public partial class Form1 : Form
+    {
+        static readonly string cyrillic = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя0123456789";
+        static readonly string latin = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string text = textBox1.Text;
+                string key = textBox3.Text;
+                bool lang = checkBox1.Checked; // if true -> cyrillic otherwise -> latin
+
+                if (lang)
+                {
+                    Is_key_correct(key, cyrillic);
+                    Is_text_correct(text, latin);
+                    textBox2.Text = Encrypt(text, key, cyrillic);
+                }
+                else
+                {
+                    Is_key_correct(key, latin);
+                    Is_text_correct(text, cyrillic);
+                    textBox2.Text = Encrypt(text, key, latin);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+                textBox2.Clear();
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string text = textBox2.Text;
+                string key = textBox3.Text;
+                bool lang = checkBox1.Checked; // if true -> cyrillic otherwise -> latin
+
+                if (lang)
+                {
+                    Is_key_correct(key, cyrillic);
+                    Is_text_correct(text, latin);
+                    textBox1.Text = Decrypt(text, key, cyrillic);
+                }
+                else
+                {
+                    Is_key_correct(key, latin);
+                    Is_text_correct(text, cyrillic);
+                    textBox1.Text = Decrypt(text, key, latin);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+                textBox1.Clear();
+            }
+        }
+
+        private static string Encrypt(string text, string key, string alphabet)
+        {
+            try
+            {
+                var result = new StringBuilder();
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (alphabet.Contains(text[i]))
+                    {
+                        result.Append(alphabet[
+                            (alphabet.IndexOf(text[i]) + alphabet.IndexOf(key[i % key.Length])) % alphabet.Length
+                        ]);
+                    }
+                    else
+                    {
+                        result.Append(text[i]);
+                    }
+                }
+
+                return result.ToString();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error");
+                return "Error";
+            }
+        }
+
+        private static string Decrypt(string text, string key, string alphabet)
+        {
+            try
+            {
+                var result = new StringBuilder();
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (alphabet.Contains(text[i]))
+                    {
+                        result.Append(alphabet[
+                            (alphabet.IndexOf(text[i]) + alphabet.Length - alphabet.IndexOf(key[i % key.Length])) % alphabet.Length
+                        ]);
+                    } else
+                    {
+                        result.Append(text[i]);
+                    }
+                }
+
+                return result.ToString();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Error");
+                return "Error";
+            }
+        }
+
+        private static void Is_key_correct(string key, string alphabet)
+        {
+            alphabet = alphabet.Replace("0123456789", "");
+
+            foreach (char s in key)
+            {
+                if (!alphabet.Contains(s))
+                {
+                    throw new Exception("Ключ должен состоять из соответствующих алфавиту букв и не содержать пробелов.");
+                }
+            }
+        }
+
+        private static void Is_text_correct(string text, string alphabet)
+        {
+            string oppositeAlphabet = alphabet.Replace("0123456789", "");
+            foreach (char s in text)
+            {
+                if (s != ' ' && oppositeAlphabet.Contains(s))
+                {
+                    throw new Exception("Текст содержит не соответствующие алфавиту буквы.");
+                }
+            }
+        }
+    }
+}
